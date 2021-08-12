349fc7eec0833bdfd1d92737b681b4c502f3ad8e
Alexander Hutoezki <huto@wa-gruppe.de>
2021-08-12 17:25:37 +0200

    chore: Improve Datetime formatter

--- src/DateFormatter.php
+++ src/DateFormatter.php
@@ -6,16 +6,52 @@ namespace Waglpz\View\Helpers;
 
 final class DateFormatter
 {
-    private \DateTimeInterface $dateTime;
+    private \DateTimeInterface $datetime;
     private string $pattern;
 
     /**
-     * for pattern values see http://userguide.icu-project.org/formatparse/datetime#TOC-DateTimePatternGenerator
+     * for pattern values see
+     * http://userguide.icu-project.org/formatparse/datetime#TOC-DateTimePatternGenerator
+     *
+     * @param string|int|\DateTimeInterface|mixed|null $datetime
      */
-    public function __construct(\DateTimeInterface $dateTime, string $pattern = 'd.MMMM.yyyy')
+    public function __construct($datetime, string $pattern = 'dd MMM yyyy HH:mm')
     {
-        $this->dateTime = $dateTime;
-        $this->pattern  = $pattern;
+        $this->pattern = $pattern;
+
+        if ($datetime instanceof \DateTimeInterface) {
+            $this->datetime = $datetime;
+
+            return;
+        }
+
+        if ($datetime === null) {
+            $this->datetime = new \DateTimeImmutable();
+
+            return;
+        }
+
+        if (\is_int($datetime)) {
+            $this->datetime = (new \DateTimeImmutable())->setTimestamp($datetime);
+
+            return;
+        }
+
+        if (! \is_string($datetime)) {
+            throw new \InvalidArgumentException(
+                'Invalid "$dateTime" given. One of int, null, string or DatetimeImmutable type is valid.'
+            );
+        }
+
+        $datetime = \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $datetime);
+
+        if ($datetime === false) {
+            throw new \InvalidArgumentException(
+                'Invalid "$dateTime" can not create Datetime Instance from given value.'
+            );
+        }
+
+        $this->datetime = $datetime;
     }
 
     public function __toString(): string
@@ -26,13 +62,15 @@ final class DateFormatter
             \IntlDateFormatter::SHORT
         );
         $formatter->setPattern($this->pattern);
-
-        $formatted = $formatter->format($this->dateTime);
+        /** @phpstan-var string $formatted */
+        $formatted = $formatter->format($this->datetime);
         if ($formatter->getErrorCode() !== \U_ZERO_ERROR) {
-            throw new \RuntimeException($formatter->getErrorMessage(), $formatter->getErrorCode());
+            throw new \RuntimeException(
+                $formatter->getErrorMessage(),
+                $formatter->getErrorCode()
+            );
         }
 
-        /** @noinspection MagicMethodsValidityInspection */
         return $formatted;
     }
 }
---
+++ tests/DateFormatterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Waglpz\View\Helpers\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Waglpz\View\Helpers\DateFormatter;
+
+final class DateFormatterTest extends TestCase
+{
+    /**
+     * @param mixed $datetime
+     * @param mixed $expectation
+     *
+     * @test
+     * @dataProvider validDateTimes
+     */
+    public function formatOk($datetime, $expectation): void
+    {
+        $sut = new DateFormatter($datetime);
+        self::assertEquals($expectation, (string) $sut);
+    }
+
+    /** @return \Generator<mixed> */
+    public function validDateTimes(): \Generator
+    {
+        $formatter = new \IntlDateFormatter(
+            \Locale::getDefault(),
+            \IntlDateFormatter::SHORT,
+            \IntlDateFormatter::SHORT
+        );
+        $formatter->setPattern('dd MMM yyyy HH:mm');
+
+        yield 'string 2044-04-04 14:44:44' => ['2044-04-04 14:44:44', '04 Apr 2044 14:44'];
+        yield 'null' => [null, $formatter->format(new \DateTimeImmutable())];
+        yield 'int' => [1628780154, '12 Aug 2021 14:55'];
+        yield 'instance' => [new \DateTimeImmutable(), $formatter->format(new \DateTimeImmutable())];
+    }
+
+    /**
+     * @param mixed $datetime
+     *
+     * @test
+     * @dataProvider invalidDateTimes
+     */
+    public function throwsException($datetime): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Invalid "$dateTime" can not create Datetime Instance from given value.'
+        );
+        new DateFormatter($datetime);
+    }
+
+    /** @return \Generator<mixed> */
+    public function invalidDateTimes(): \Generator
+    {
+        yield 'string 2044-04-100' => ['2044-04-100'];
+        yield 'string wrong' => ['wrong'];
+        yield 'string 100' => ['100'];
+    }
+}
